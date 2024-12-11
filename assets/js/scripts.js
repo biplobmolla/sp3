2614289d2787ae365383f1857ba4d5b89edab84c
biplobmolla01 <69385735+biplobmolla@users.noreply.github.com>
2024-12-11 11:28:23 +0600

    Hello

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,21 @@
 const cards = document.querySelectorAll(".card");
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        location.href = "./product-details.html";
-    });
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    location.href = "./product-details.html";
+  });
+});
+
+const smallCards = document.querySelectorAll(".small-card");
+
+smallCards.forEach((card) => {
+  card.addEventListener("click", () => {
+    location.href = "./category.html";
+  });
+});
+
+const buyNowBtn = document.querySelector("#buy");
+
+buyNowBtn.addEventListener("click", () => {
+  location.href = "./checkout.html";
 });
